e7fea1d1c6c55832f0e5010c021ac4f77cc23fb0
Abiel Mainty <maintabi001@warren.k12.in.us>
2024-10-30 07:23:59 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# TODO: Write something in the
+#  input function
+#  instructing the user to enter their age.
+age = int(input("What is your age? "))
+
+#TODO: use a relational operator to evaluate is_elegible
+# YOU are are elegible if your age is greater than or equal
+# to 18.
+is_eligible = age >= 18
+
+#TODO: print the result
+print("You are eligible to vote: ", is_eligible)
+
+
